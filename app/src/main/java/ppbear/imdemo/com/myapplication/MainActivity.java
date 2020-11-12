858f77b8eeac1a294bfe4ddad89a8b8f9d2c5e1c
ppbear <ppbear_ly@163.com>
2020-11-12 15:11:45 +0800

    first submit

---
+++ app/src/main/java/ppbear/imdemo/com/myapplication/MainActivity.java
@@ -0,0 +1,270 @@
+package ppbear.imdemo.com.myapplication;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.graphics.SurfaceTexture;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.CaptureResult;
+import android.hardware.camera2.TotalCaptureResult;
+import android.hardware.camera2.params.OutputConfiguration;
+import android.hardware.camera2.params.StreamConfigurationMap;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.util.Size;
+import android.view.Surface;
+import android.view.TextureView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, TextureView.SurfaceTextureListener {
+
+    private TextView textView;
+    private EditText heightEditText, widthEditText;
+    private Button switchBtn;
+    private TextureView textureView;
+    CaptureRequest.Builder texttureViewBuilder;
+    HandlerThread handlerThread ;
+    Handler handler;
+    SurfaceTexture texture;
+    Surface surface;
+    private Size mPreviewSize;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+        handlerThread = new HandlerThread("CAMERA2");
+        handlerThread.start();
+        handler = new Handler(handlerThread.getLooper());
+    }
+
+    public void initView() {
+        textView = findViewById(R.id.tv_support_dimen);
+        heightEditText = findViewById(R.id.et_height);
+        widthEditText = findViewById(R.id.et_width);
+        switchBtn = findViewById(R.id.btn_switch);
+        switchBtn.setOnClickListener(this);
+        textureView = findViewById(R.id.ttv_camera);
+        textureView.setSurfaceTextureListener(this);
+
+    }
+
+    public volatile boolean flag = true;
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_switch:
+                try {
+                    flag = false;
+
+                    texture.setDefaultBufferSize(Integer.parseInt(widthEditText.getText().toString()),Integer.parseInt(heightEditText.getText().toString()));
+                    //textureView.setSurfaceTextureListener(this);
+                    surface = new Surface(texture);
+                    startPreview(cameraDevice);
+                } catch (CameraAccessException e) {
+                    e.printStackTrace();
+                }
+                break;
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    private void startPreview(CameraDevice camera) throws CameraAccessException {
+
+        if(cameraDevice == null){
+            texture = textureView.getSurfaceTexture();
+
+            texture.setDefaultBufferSize(textureView.getWidth(), textureView.getHeight());
+            surface = new Surface(texture);
+        }
+
+        try {
+            texttureViewBuilder = camera.createCaptureRequest(CameraDevice.TEMPLATE_STILL_CAPTURE);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+        texttureViewBuilder.addTarget(surface);
+        camera.createCaptureSession(Arrays.asList(surface), mSessionStateCallback, handler);
+
+    }
+
+    private CameraCaptureSession.StateCallback mSessionStateCallback = new CameraCaptureSession.StateCallback() {
+        @Override
+        public void onConfigured(CameraCaptureSession session) {
+            try {
+                //session.capture(mPreviewBuilder.build(), mSessionCaptureCallback, mHandler);
+                session.setRepeatingRequest(texttureViewBuilder.build(), mSessionCaptureCallback, handler);
+            } catch (CameraAccessException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void onConfigureFailed(@NonNull CameraCaptureSession session) {
+
+        }
+    };
+    CameraCaptureSession cameraCaptureSession;
+    private CameraCaptureSession.CaptureCallback mSessionCaptureCallback =
+            new CameraCaptureSession.CaptureCallback() {
+
+                @Override
+                public void onCaptureCompleted(CameraCaptureSession session, CaptureRequest request,
+                                               TotalCaptureResult result) {
+                    try {
+                        if(flag) {
+                            cameraCaptureSession = session;
+
+                            Log.i("test","--------------切换分辨率之前--------------");
+                            session.setRepeatingRequest(texttureViewBuilder.build(), mSessionCaptureCallback, handler);
+                        }else{
+                            Log.i("test","--------------切换分辨率之后--------------");
+                            //session.setRepeatingRequest(texttureViewBuilder.build(), mSessionCaptureCallback, handler);
+                        }
+                    } catch (CameraAccessException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                @Override
+                public void onCaptureStarted(@NonNull CameraCaptureSession session, @NonNull CaptureRequest request, long timestamp, long frameNumber) {
+                    super.onCaptureStarted(session, request, timestamp, frameNumber);
+                }
+
+                @Override
+                public void onCaptureProgressed(CameraCaptureSession session, CaptureRequest request,
+                                                CaptureResult partialResult){}};
+
+    @SuppressWarnings("ResourceType")
+    @Override
+    public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+        CameraManager cameraManager = (CameraManager) getSystemService(CAMERA_SERVICE);
+        try {
+            String[] CameraIdList = cameraManager.getCameraIdList();
+            //获取可用相机设备列表
+            CameraCharacteristics characteristics = cameraManager.getCameraCharacteristics(CameraIdList[0]);
+            //在这里可以通过CameraCharacteristics设置相机的功能,当然必须检查是否支持
+            characteristics.get(CameraCharacteristics.INFO_SUPPORTED_HARDWARE_LEVEL);
+            //就像这样
+
+            StreamConfigurationMap map = characteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
+            Log.i("test","当前摄像机分辨率：");
+            cameraManager.openCamera(CameraIdList[0], mCameraDeviceStateCallback, handler);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+
+    }
+
+    @Override
+    public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+        return false;
+    }
+
+    @Override
+    public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+
+    }
+    CameraDevice cameraDevice;
+    private CameraDevice.StateCallback mCameraDeviceStateCallback = new CameraDevice.StateCallback() {
+        @Override
+        public void onOpened(CameraDevice camera) {
+            try {
+                startPreview(camera);
+                cameraDevice = camera;
+            } catch (CameraAccessException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void onDisconnected(CameraDevice camera) {}
+
+        @Override
+        public void onError(CameraDevice camera, int error) {}
+    };
+
+    /**
+    * Editer: ppbear
+    * Edit By 2020/5/10 0:12
+    * Function:
+    * Return:
+    * Description: 设置预览view及分辨率大小
+    */
+    private static Size chooseOptimalSize(Size[] choices, int textureViewWidth,
+                                          int textureViewHeight, int maxWidth, int maxHeight, Size aspectRatio) {
+
+        // Collect the supported resolutions that are at least as big as the preview Surface
+        List<Size> bigEnough = new ArrayList<>();
+        // Collect the supported resolutions that are smaller than the preview Surface
+        List<Size> notBigEnough = new ArrayList<>();
+        int w = aspectRatio.getWidth();
+        int h = aspectRatio.getHeight();
+        for (Size option : choices) {
+            if (option.getWidth() <= maxWidth && option.getHeight() <= maxHeight &&
+                    option.getHeight() == option.getWidth() * h / w) {
+                if (option.getWidth() >= textureViewWidth &&
+                        option.getHeight() >= textureViewHeight) {
+                    bigEnough.add(option);
+                } else {
+                    notBigEnough.add(option);
+                }
+            }
+        }
+
+        // Pick the smallest of those big enough. If there is no one big enough, pick the
+        // largest of those not big enough.
+        if (bigEnough.size() > 0) {
+            return Collections.min(bigEnough, new CompareSizesByArea());
+        } else if (notBigEnough.size() > 0) {
+            return Collections.max(notBigEnough, new CompareSizesByArea());
+        } else {
+            Log.e("Camera", "Couldn't find any suitable preview size");
+            return choices[0];
+        }
+    }
+
+    /**
+     * Compares two {@code Size}s based on their areas.
+     */
+    static class CompareSizesByArea implements Comparator<Size> {
+
+        @Override
+        public int compare(Size lhs, Size rhs) {
+            // We cast here to ensure the multiplications won't overflow
+            return Long.signum((long) lhs.getWidth() * lhs.getHeight() -
+                    (long) rhs.getWidth() * rhs.getHeight());
+        }
+
+    }
+
+
+
+}
